133a67c1dddfea7fa5c24182a170d05c5c9b47e5
rezivo <therapio@proton.me>
2026-06-05 14:08:51 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@ const REZIVO_SUPABASE_URL = 'https://trjofnazwjhehssirghm.supabase.co';
 const REZIVO_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_9njtTqvxxsJ3JQsvmWmgHA_OS8bCDUQ';
 
 /* BLOC CLIENT SUPABASE - fără localStorage */
-const rezivoSupabase = window.supabase.createClient(
+window.rezivoSupabase = window.supabase.createClient(
   REZIVO_SUPABASE_URL,
   REZIVO_SUPABASE_PUBLISHABLE_KEY,
   {
